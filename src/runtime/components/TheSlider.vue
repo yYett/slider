2152e502aa79ea9f7cc2f9cc67cedb2f7c66eec9
yYett <56874009+yYett@users.noreply.github.com>
2023-09-11 15:38:56 +0100

    feature: components

---
+++ src/runtime/components/TheSlider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="the-slider">
+    <div class="the-items">
+      <slot name="items" />
+    </div>
+
+    <slot name="navigation">
+      <div class="the-navigation-prev" />
+      <div class="the-navigation-next" />
+    </slot>
+    <slot name="pagination">
+      <div class="the-slider-pagination" />
+    </slot>
+  </div>
+</template>
+<script setup></script>
---
+++ src/runtime/components/TheSliderItem.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="the-slider">
+    <slot />
+  </div>
+</template>
+<script setup></script>
