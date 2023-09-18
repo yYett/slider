e545abcf262cee2c531679e3fc51c67a5b71a7d1
yYett <56874009+yYett@users.noreply.github.com>
2023-09-18 19:33:14 +0100

    refactor: update debouce - ts

--- src/runtime/utils/commons.ts
+++ src/runtime/utils/commons.ts
@@ -1,12 +1,11 @@
-export function debounce(fn, wait) {
-  let timer;
-  return function (...args) {
-    if (timer) {
-      clearTimeout(timer); // clear any pre-existing timer
-    }
-    const context = this; // get the current context
-    timer = setTimeout(() => {
-      fn.apply(context, args); // call the function if time expires
-    }, wait);
+export function debounce<T extends (...args: Parameters<T>) => void>(
+  this: ThisParameterType<T>,
+  fn: T,
+  delay = 300
+) {
+  let timer: ReturnType<typeof setTimeout> | undefined;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), delay);
   };
 }
